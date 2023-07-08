eddf9e02dc19b1c016f411f3c71067c57b897ce6
Joey <joey@joeyh.dev>
2023-07-08 23:36:39 +0000

    update scripts

--- scripts/mkrootfs.sh
+++ scripts/mkrootfs.sh
@@ -1,25 +1,41 @@
+#! /bin/bash
 # pass dockerfile as $1
-dockerfile="$1"
+lang="$1"
 
 # check if command line argument is there
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "provide language for which to build rootfs"
     exit 0
 fi
 
+# 100MB rootfs - change if gonna run out of space
+
+case $lang in
+"rust")
+    size=900
+    ;;
+"python")
+    size=100
+    ;;
+*)
+    echo "unrecognised language"
+    exit 0
+    ;;
+esac
+
+
 # build pyrod
 cargo build --release --bin=pyrod --target=x86_64-unknown-linux-musl
 
-# 100MB rootfs - change if gonna run out of space
-dd if=/dev/zero of=rootfs.ext4 bs=1M count=100
+dd if=/dev/zero of=rootfs.ext4 bs=1M count=$size
 mkfs.ext4 rootfs.ext4
 
 
 sudo rm -rf /tmp/rootfs && mkdir /tmp/rootfs
 sudo mount rootfs.ext4 /tmp/rootfs
 
-sudo docker build --no-cache . -f scripts/images/Dockerfile.$1 -t pyro-$1
-sudo docker run -it --rm -v /tmp/rootfs:/rootfs pyro-$1
+sudo docker build --progress=plain --no-cache . -f "scripts/images/Dockerfile.$lang" -t "pyro-$lang"
+sudo docker run -it --rm -v /tmp/rootfs:/rootfs "pyro-$lang"
 
 sudo umount /tmp/rootfs
-mv rootfs.ext4 resources/rootfs-$1.ext4
+mv rootfs.ext4 "resources/rootfs-$lang.ext4"
