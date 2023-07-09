66fef880bf8b3cfbc00ec877f54606858233e484
Joey <joey@joeyh.dev>
2023-07-09 16:20:39 +0000

    add api

--- pyromaniac/src/api/mod.rs
+++ pyromaniac/src/api/mod.rs
@@ -1,12 +1,87 @@
+use std::{sync::Arc, time::Duration};
+
+use crate::runner::run_code;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Result},
+    routing::post,
+    Json, Router,
+};
+use tokio::sync::Semaphore;
+
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct CodeRun {
     code: String,
-    input: String,
+    input: Option<String>,
     lang: pyrod_service::Language,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct CodeResult {
+struct CodeOutput {
     stdout: String,
     stderr: String,
+}
+
+#[tracing::instrument]
+async fn run(
+    State(semaphore): State<Arc<Semaphore>>,
+    Json(req): Json<CodeRun>,
+) -> Result<Json<CodeOutput>> {
+    //there's two things done here to bound the number of VMs running
+
+    //Tasks are spawned with a timeout of 30 seconds.
+    //after that the task is dropped, therefore machine dropped and process killed
+
+    //We also have a Semaphore. Tasks must acquire a permit from the semaphore
+    //before being allowed to run. This limits the number of VMs active at any time.
+
+    //this does mean requests may take a while, so we need to set HTTP timeouts appropriately
+
+    let _permit = semaphore
+        .acquire_owned()
+        .await
+        .map_err(|e| JsonError::from(e).into_response())?;
+
+    let (stdout, stderr) = tokio::time::timeout(
+        Duration::from_secs(30),
+        run_code(req.lang, req.code, req.input.unwrap_or("".to_string())),
+    )
+    .await
+    .map_err(|e| JsonError::from(e).into_response())? // Result::flatten is nightly still
+    .map_err(|e| JsonError::from(e).into_response())?;
+
+    Ok(Json(CodeOutput { stdout, stderr }))
+}
+
+pub fn app() -> Router {
+    //you really expect anyone to handle this error?
+    let n_cpus = std::thread::available_parallelism()
+        .expect("Failed to determine number of CPUs")
+        .get();
+    let max_vms = n_cpus * 2;
+
+    Router::new()
+        .route("/api/run", post(run))
+        .with_state(Arc::new(Semaphore::new(max_vms)))
+        .fallback(|| async { (StatusCode::NOT_FOUND, "Not Found\n") })
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct JsonError {
+    error: String,
+}
+
+impl<E: ToString> From<E> for JsonError {
+    fn from(value: E) -> Self {
+        Self {
+            error: value.to_string(),
+        }
+    }
+}
+
+impl IntoResponse for JsonError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, axum::Json(self)).into_response()
+    }
 }
--- pyromaniac/src/main.rs
+++ pyromaniac/src/main.rs
@@ -1,42 +1,21 @@
-use pyrod_service::Language;
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
-
 mod api;
 mod config;
 mod runner;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     tracing::subscriber::set_global_default(
         tracing_subscriber::fmt::fmt()
             .with_span_events(FmtSpan::ACTIVE)
             .with_max_level(Level::DEBUG)
             .finish(),
-    )?;
-
-    //     let script = r"
-    // import sys
-    // print('Hello, world!')
-    // print('Hello, stderr!', file=sys.stderr)
-    // print('Hello, stdin:')
-    // for line in sys.stdin:
-    //     line = line.rstrip()
-    //     print(f'Message from stdin: {line}')
-    //     ";
-
-    let rust = "
-fn main() {
-    println!(\"Hello, world\");
-}
-    ";
-
-    let output = runner::run_code(
-        Language::Rust,
-        rust.to_owned(),
-        "line 1\n line2\n line3".to_owned(),
     )
-    .await?;
-    dbg!(output);
-    Ok(())
+    .expect("Could not install tracing subscriber");
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(api::app().into_make_service())
+        .await
+        .expect("Could not start server");
 }
